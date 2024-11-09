7463fef941d0fc2d59a3c262ff9d3cee72ee0b9d
Andrey Ledovskoy <ledovskoy@cvisionlab.com>
2024-11-09 21:21:06 +0300

    added tb and clearml loggers

--- scripts/train.py
+++ scripts/train.py
@@ -3,6 +3,8 @@ from pathlib import Path
 
 import yaml
 import lightning as L
+from clearml import Task
+from lightning.pytorch.loggers import TensorBoardLogger
 from segm_pro.train import (
     SegmentationModule,
     SegmDSModule,
@@ -19,6 +21,10 @@ def parse_args():
         '--config', type=Path, required=True,
         help='Path to train config *.yaml'
     )
+    parser.add_argument(
+        '--clearml', action='store_true',
+        help='If True, then logging to clearml'
+    )
     return parser.parse_args()
 
 
@@ -26,6 +32,13 @@ def main():
     args = parse_args()
     with args.config.open('r') as file:
         config = yaml.safe_load(file)
+
+    logger = None
+    if args.clearml:
+        Task.init(
+            project_name="segm-pro", task_name="test_launch"
+        )
+        logger = TensorBoardLogger("tb_logs", name="test_launch")
 
     train_params = TrainParams(**config['train'])
     data_params = DataParams(**config['data'])
@@ -35,7 +48,8 @@ def main():
 
     trainer = L.Trainer(
         gradient_clip_val=0.5,
-        gradient_clip_algorithm="value"
+        gradient_clip_algorithm="value",
+        logger=logger
     )
     trainer.fit(model, datamodule=data_module)
 
