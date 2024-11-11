f2372e8c7732c407d8f608cd4e235fa0f69a6c22
Andrey Ledovskoy <ledovskoy@cvisionlab.com>
2024-11-11 21:22:17 +0300

    added augmentation

--- src/segm_pro/train/data_module.py
+++ src/segm_pro/train/data_module.py
@@ -59,17 +59,13 @@ class SegmDSModule(L.LightningDataModule):
     def __get_train_transform(self):
         return A.Compose([
             A.Resize(768, 768),
-            # A.ShiftScaleRotate(
-            #     shift_limit=0.2, scale_limit=0.2,
-            #     rotate_limit=30, p=0.5
-            # ),
-            # A.RGBShift(
-            #     r_shift_limit=25, g_shift_limit=25,
-            #     b_shift_limit=25, p=0.5
-            # ),
-            # A.RandomBrightnessContrast(
-            #     brightness_limit=0.3, contrast_limit=0.3, p=0.5
-            # ),
+            A.RGBShift(
+                r_shift_limit=25, g_shift_limit=25,
+                b_shift_limit=25, p=0.5
+            ),
+            A.RandomBrightnessContrast(
+                brightness_limit=0.3, contrast_limit=0.3, p=0.5
+            ),
             A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
             ToTensorV2(),
         ])
