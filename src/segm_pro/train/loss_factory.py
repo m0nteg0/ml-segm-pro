302db66415b9570866563b2d0aa91fa46827fb58
Andrey Ledovskoy <ledovskoy@cvisionlab.com>
2024-11-18 11:58:48 +0300

    added docstrings

--- src/segm_pro/train/loss_factory.py
+++ src/segm_pro/train/loss_factory.py
@@ -1,3 +1,5 @@
+"""Factory of segmentation model losses."""
+
 from enum import Enum
 
 import torch
@@ -14,12 +16,32 @@ from segmentation_models_pytorch.losses import (
 
 
 class LossMode(Enum):
+    """Enum defining the available loss modes for segmentation tasks."""
+
     BINARY = constants.BINARY_MODE
     MULTICLASS = constants.MULTICLASS_MODE
 
 
 class CommonCE(nn.Module):
+    """Common cross-entropy loss implementation for segmentation tasks.
+
+    This class provides a simple way to select between binary and multi-class
+    cross-entropy losses based on the provided mode.
+    """
+
     def __init__(self, mode: str):
+        """Initialize loss.
+
+        Parameters
+        ----------
+        mode : str
+            Supported values are:
+            - LossMode.BINARY.value:  Binary segmentation using 
+            SoftBCEWithLogitsLoss.
+            - LossMode.MULTICLASS.value: Multiclass segmentation using 
+            SoftCrossEntropyLoss.
+
+        """
         super().__init__()
         self._loss = (
             SoftBCEWithLogitsLoss()
@@ -32,10 +54,29 @@ class CommonCE(nn.Module):
             y_pred: torch.Tensor,
             y_true: torch.Tensor
     ) -> torch.Tensor:
+        """Compute the loss between predicted and true segmentation masks.
+
+        Parameters
+        ----------
+        y_pred : torch.Tensor
+            A tensor of shape [batch_size, num_classes, height, width]
+            representing the predicted segmentation masks.
+        y_true : torch.Tensor
+            A tensor of shape [batch_size, height, width]
+            representing the true segmentation masks.
+
+        Returns
+        -------
+        torch.Tensor
+            The calculated loss value.
+
+        """
         return self._loss(y_pred, y_true)
 
 
 class LossType(Enum):
+    """Enum defining different loss functions used for segmentation tasks."""
+
     CE = 'CELoss'
     IOU = 'JaccardLoss'
     DICE = 'DiceLoss'
@@ -44,6 +85,13 @@ class LossType(Enum):
 
 
 class ComplexLoss(nn.Module):
+    """Combines multiple segmentation loss types with customizable weights.
+
+    This class allows you to define a complex loss function by specifying
+    a combination of different segmentation loss types and their respective
+    weights.
+    """
+
     def __init__(
             self,
             losses: tuple[LossType, ...],
@@ -78,6 +126,26 @@ class ComplexLoss(nn.Module):
             y_pred: torch.Tensor,
             y_true: torch.Tensor
     ) -> torch.Tensor:
+        """Calculate the total loss by combining individual losses.
+
+        This method applies a set of defined losses to the predicted
+        (`y_pred`) and true (`y_true`) tensors, each weighted according
+        to their importance specified in `self._weights`. The individual
+        losses are then summed to produce the overall loss value.
+
+        Parameters
+        ----------
+        y_pred : torch.Tensor
+            Predicted segmentation map tensor
+        y_true : torch.Tensor
+            True segmentation map tensor.
+
+        Returns
+        -------
+        torch.Tensor
+            Total loss value as a single tensor.
+
+        """
         losses = torch.stack([
             loss(y_pred, y_true) * w
             for loss, w in zip(self._losses, self._weights)
@@ -89,5 +157,27 @@ def create_loss(
         losses: tuple[LossType, ...],
         weights: tuple[float, ...],
         loss_mode: LossMode
-):
+) -> ComplexLoss:
+    """Create a complex loss function combining multiple loss types.
+
+    Parameters
+    ----------
+    losses : tuple[LossType, ...]
+        A tuple of desired loss types to combine. 
+        Each element should be a LossType enum value, such as
+        LossType.CE, LossType.IOU, etc.
+    weights : tuple[float, ...]
+        A tuple of corresponding weights for each loss type.
+        The weights determine the contribution of each loss to the
+        final output.
+    loss_mode : LossMode
+        The mode of the classification, either BINARY or MULTICLASS.
+
+    Returns
+    -------
+    ComplexLoss
+        An instance of ComplexLoss that combines the specified losses 
+        and weights.
+
+    """
     return ComplexLoss(losses, weights, loss_mode)
--- src/segm_pro/train/metrics_factory.py
+++ src/segm_pro/train/metrics_factory.py
@@ -1,5 +1,8 @@
+"""Factory of segmentation model metrics."""
+
 from enum import Enum
 
+from torchmetrics import Metric
 from torchmetrics.segmentation import (
     MeanIoU,
     GeneralizedDiceScore,
@@ -8,12 +11,32 @@ from torchmetrics.segmentation import (
 
 
 class MetricType(Enum):
+    """Enum defining the metrics for segmentation model evaluation."""
+
     IOU = 'MeanIoU'
     DICE = 'Dice'
     HD = 'HausdorffDistance'
 
 
-def create_metric(metric_type: MetricType, num_classes: int = 1):
+def create_metric(metric_type: MetricType, num_classes: int = 1) -> Metric:
+    """Create a segmentation metric based on the specified type.
+
+    Parameters
+    ----------
+    metric_type : MetricType
+        The type of metric to create. Supported types are:
+        - MetricType.IOU: Mean Intersection over Union (mIoU)
+        - MetricType.DICE: Generalized Dice Score
+        - MetricType.HD: Hausdorff Distance
+    num_classes : int, optional
+        The number of classes in the segmentation task, by default 1
+
+    Returns
+    -------
+    Metric
+        An instance of the selected metric class.
+
+    """
     fabric = {
         MetricType.IOU.value: MeanIoU,
         MetricType.DICE.value: GeneralizedDiceScore,
